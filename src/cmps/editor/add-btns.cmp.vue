23319e82a333faff54405aaf30ae9eead0ace640
Naphtali Rubin <naphtali.r.89@gmail.com>
2020-11-24 20:41:27 +0200

    added a dashboard

---
+++ src/cmps/editor/add-btns.cmp.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="edit-btns">
+        <h1>This is edit mode</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'edit-btns',
+    data() {
+        return {
+        }
+    },
+
+    components: {
+
+    }
+}
+</script>
