a6e88b1e827792f11e6bd83c4a4088f883b80cd6
Matan Cohen <matanco3605@gmail.com>
2020-11-24 23:01:03 +0200

    unsplash gal

---
+++ src/cmps/editor/unsplash-gallery.cmp.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="unsplash-gallery">
+        <photo-preview v-for="(photo,idx) in photos" :key="idx" :src="photo.thumb" />
+    </section>
+</template>
+
+<script>
+import photoPreview from '@/cmps/editor/photo-preview.cmp.vue'
+export default {
+    name: 'unsplash-gallery',
+    props: {
+		photos: Array
+    },
+    components:{
+        photoPreview,
+    }
+}
+</script>
+
+<style>
+
+</style>
+
