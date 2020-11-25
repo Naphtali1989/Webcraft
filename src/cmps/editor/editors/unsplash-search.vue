48bcb40b0e7aff20ef080126adc15da7953d5ada
Idanatiya <idanatiya122@gmail.com>
2020-11-25 22:55:54 +0200

    aa

--- src/cmps/editor/editors/unsplash-search.vue
+++ src/cmps/editor/editors/unsplash-search.vue
@@ -5,11 +5,7 @@
             <button>Submit</button>
         </form>
         <div class="unsplash-imgs-grid">
-            <div
-                class="unsplash-img-card"
-                v-for="(photo, idx) in photos"
-                :key="idx"
-            >
+            <div class="unsplash-img-card" v-for="(photo, idx) in photos" :key="idx">
                 <img class="unsplash-img" :src="photo.thumb" />
             </div>
         </div>
@@ -26,16 +22,16 @@ export default {
     },
     methods: {
         async searchPhotos() {
-            const apiKey = 'DL-fOJKfzUbQ2irbF2Oleeza3GuX2LyJ-mVPCUJsJc8';
-            const res = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${this.term}&client_id=${apiKey}`)
-            const data = await res.json()
-            console.log('data from api:', data);
-            const photos = this.getUrls(data.results);
-            this.photos = photos;
+            const apiKey='DL-fOJKfzUbQ2irbF2Oleeza3GuX2LyJ-mVPCUJsJc8';
+            const res=await fetch(`https://api.unsplash.com/search/photos?page=1&query=${this.term}&client_id=${apiKey}`)
+            const data=await res.json()
+            console.log('data from api:',data);
+            const photos=this.getUrls(data.results);
+            this.photos=photos;
         },
         getUrls(results) {
-            const res = results.map(result => result.urls)
-            console.log('res:', res);
+            const res=results.map(result => result.urls)
+            console.log('res:',res);
             return res;
         }
     },
--- src/views/editor.cmp.vue
+++ src/views/editor.cmp.vue
@@ -13,7 +13,8 @@ export default {
     data() {
         return {
             cmps: null,
-            currCmpToEdit: null
+            currCmpToEdit: null,
+            currWep: null
         }
     },
     computed: {
