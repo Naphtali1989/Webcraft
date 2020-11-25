d1239f2660b6fbdc971fdd6599df47c4f4af0e2e
unknown <idanatiya122@gmail.com>
2020-11-25 02:45:43 +0200

    get

---
+++ src/cmps/samples/header-samples/header-sample2.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header-container-2">
+      <nav>
+          <div class="logo">
+            <img src="" alt="">
+            <h1>Dev ed</h1>
+          </div>
+
+          <div class="links">
+              <router-link to="">Home</router-link>
+              <router-link to="">Projects</router-link>
+              <router-link to="">Contact</router-link>
+          </div>
+
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
