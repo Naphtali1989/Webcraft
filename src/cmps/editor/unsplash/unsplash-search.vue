5ce86085423a34602b04274670a266246ced0d1c
Idanatiya <idanatiya122@gmail.com>
2020-11-25 20:26:08 +0200

    ko

--- src/cmps/editor/unsplash/unsplash-search.vue
+++ src/cmps/editor/unsplash/unsplash-search.vue
@@ -4,6 +4,9 @@
             <input type="text" placeholder="Search photo..." v-model="term">
             <button>Submit</button>
         </form>
+        <div class="grid-unsplash-imgs">
+
+        </div>
         <unsplash-gallery :photos="this.photos">
     </section>
 </template>
@@ -20,16 +23,16 @@ export default {
     },
     methods: {
         async searchPhotos() {
-            const apiKey = 'DL-fOJKfzUbQ2irbF2Oleeza3GuX2LyJ-mVPCUJsJc8';
-            const res = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${this.term}&client_id=${apiKey}`)
-            const data = await res.json()
-            console.log('data from api:', data);
-            const photos = this.getUrls(data.results);
-            this.photos = photos;
+            const apiKey='DL-fOJKfzUbQ2irbF2Oleeza3GuX2LyJ-mVPCUJsJc8';
+            const res=await fetch(`https://api.unsplash.com/search/photos?page=1&query=${this.term}&client_id=${apiKey}`)
+            const data=await res.json()
+            console.log('data from api:',data);
+            const photos=this.getUrls(data.results);
+            this.photos=photos;
         },
         getUrls(results) {
-            const res = results.map(result => result.urls)
-            console.log('res:', res);
+            const res=results.map(result => result.urls)
+            console.log('res:',res);
             return res;
         }
     },
