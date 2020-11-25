4f8ff604af446b7ef3f73df9a2fda40d595c2bfc
Matan Cohen <matanco3605@gmail.com>
2020-11-25 23:27:42 +0200

    unsplash-search good

--- src/views/playground.cmp.vue
+++ src/views/playground.cmp.vue
@@ -4,20 +4,24 @@
         <!-- <header-sample /> -->
         <!-- <grid-sample /> -->
         <!-- <navbar-sample /> -->
-        <unsplash-search></unsplash-search>
+        <unsplash-search>
+
+        </unsplash-search>
     </section>
 </template>
 
 <script>
 // import navbarSample from '@/cmps/samples/header-samples/navbar-sample.cmp.vue';
 // import GridSample from '../cmps/samples/header-samples/grid-sample.vue';
+import unsplashSearch from '@/cmps/editor/editors/unsplash-search.cmp.vue';
 export default {
     name: 'playground',
     components: {
         // headerSample,
         // hero,
         // gridSample,
-        navbarSample
+        // navbarSample,
+        unsplashSearch
 
     }
 }
