d7c759a7398df34bc4e9fbf7dbbe785203121cd8
Matan Cohen <matanco3605@gmail.com>
2020-11-25 02:44:49 +0200

    wapgallery fixedname

--- src/cmps/wap/wap-preview.cmp.vue
+++ src/cmps/wap/wap-preview.cmp.vue
@@ -1,21 +1,31 @@
 <template>
-<section>
+    <section>
         <div class="wap-preview">
-        <h3>{{wap.title}}</h3>
-            <img :src="wap.imgUrl" >
+            <h3>{{ wap.title }}</h3>
+            <img :src="wap.imgUrl" />
+            <div class="wap-prev-controls">
+                <button >save</button>
+                <button >edit</button>
+            </div>
         </div>
-</section>
+    </section>
 </template>
 
 <script>
 export default {
-    name:'wapPreview',
-    props:{
-        wap:Object
+    name: 'wapPreview',
+    props: {
+        wap: Object
+    },
+    methods: {
+        enterWap() {
+        },
+        enterEditor() {
+        }
+
     }
 }
 </script>
 
 <style>
-
 </style>
