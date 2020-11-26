08fd7fa760bfde591d1b905b52b24e40b4ec0cc0
Idanatiya <idanatiya122@gmail.com>
2020-11-26 13:28:42 +0200

    a

--- src/cmps/editor/editors/section-editor.cmp.vue
+++ src/cmps/editor/editors/section-editor.cmp.vue
@@ -4,26 +4,28 @@
         <my-range :options="{ initVal: 2, min: 0, max: 50 }" />
         <template v-if="cmpToEdit.type === 'section'">
             <p class="editor-txt">Set A Background Color</p>
-            <color-picker @changeColor="setColor"/>
+            <color-picker @changeColor="setColor" />
         </template>
 
         <p class="editor-txt">Or Add An Image As A Background</p>
         <div class="img-uploader">
-            <label class="user-input input-file"
-                ><i class="fas fa-cloud-upload-alt"></i>
+            <label class="user-input input-file"><i class="fas fa-cloud-upload-alt"></i>
                 <input class="hide" type="file" @change="emitUploadImg" />
             </label>
         </div>
+        <unsplash-search />
+
     </section>
 </template>
 
 <script>
 import myRange from '@/cmps/custum-cmps/my-range.cmp.vue';
 import colorPicker from '@/cmps/editor/color-picker.cmp.vue';
+import unsplashSearch from '@/cmps/editor/editors/unsplash-search.cmp.vue';
 export default {
     name: 'section-editor',
     props: {
-       cmpToEdit: {
+        cmpToEdit: {
             type: Object
         }
     },
@@ -34,7 +36,7 @@ export default {
     },
     methods: {
         emitUploadImg(ev) {
-            this.$emit('uploading', ev)
+            this.$emit('uploading',ev)
             //     this.$store.commit({
             //         type:'setIsLoading',
             //         isLoading: true
@@ -47,12 +49,14 @@ export default {
             //     })
         },
         setColor(color) {
-            this.cmpToEdit.style.background = color;
+            this.cmpToEdit.style.background=color;
         }
     },
     components: {
         myRange,
-        colorPicker
+        colorPicker,
+        unsplashSearch
     }
 }
 </script>
+        UnsplashSearchCmp
